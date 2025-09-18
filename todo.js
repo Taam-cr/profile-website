3e31682b47f75824700a0d901a05495378c543ec
Taam-cr <tranphanthaotam2003@gmail.com>
2025-09-18 09:14:20 +0700

    buoi 6

--- todo.js
+++ todo.js
@@ -3,13 +3,13 @@ const taskInput= document.getElementById("taskInput");
 const addBtn = document.getElementById("addBtn");
 const taskList = document.getElementById("taskList");
 
-//lấy task từ LocalStorage
+//lấy task từ LocalStorage nếu có, hoặc tạo mảng trổng
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
 //hàm render danh sách
 function renderTasks() {
     taskList.innerHTML = ""; //xóa danh danh sách cũ
-    tasks.array.forEach((task, index) => {
+    tasks.forEach((task, index) => {
         const li = document.createElement("li");
         li.textContent = task;
         
